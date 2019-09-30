5c58462110a151693638a0c2b515693ecd9f4a18
Mateusz Bukowski <mateuszbukowski13@gmail.com>
2019-09-30 22:21:35 +0200

    [Zegar binarny] - dodałem ściąę z potęg cyfry 2

--- Zegar binarny/Zegar binarny/FormularzGlowny.Designer.vb
+++ Zegar binarny/Zegar binarny/FormularzGlowny.Designer.vb
@@ -25,23 +25,24 @@ Partial Class FormularzGlowny
         Me.lblGodziny = New System.Windows.Forms.Label()
         Me.lblMinuty = New System.Windows.Forms.Label()
         Me.lblSekundy = New System.Windows.Forms.Label()
+        Me.lblPotegi2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblGodziny
         '
         Me.lblGodziny.AutoSize = True
         Me.lblGodziny.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-        Me.lblGodziny.Location = New System.Drawing.Point(21, 17)
+        Me.lblGodziny.Location = New System.Drawing.Point(13, 43)
         Me.lblGodziny.Name = "lblGodziny"
-        Me.lblGodziny.Size = New System.Drawing.Size(245, 63)
+        Me.lblGodziny.Size = New System.Drawing.Size(273, 63)
         Me.lblGodziny.TabIndex = 0
-        Me.lblGodziny.Text = "H: 00000"
+        Me.lblGodziny.Text = "H:   00000"
         '
         'lblMinuty
         '
         Me.lblMinuty.AutoSize = True
         Me.lblMinuty.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-        Me.lblMinuty.Location = New System.Drawing.Point(12, 80)
+        Me.lblMinuty.Location = New System.Drawing.Point(4, 106)
         Me.lblMinuty.Name = "lblMinuty"
         Me.lblMinuty.Size = New System.Drawing.Size(281, 63)
         Me.lblMinuty.TabIndex = 1
@@ -51,17 +52,28 @@ Partial Class FormularzGlowny
         '
         Me.lblSekundy.AutoSize = True
         Me.lblSekundy.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-        Me.lblSekundy.Location = New System.Drawing.Point(21, 144)
+        Me.lblSekundy.Location = New System.Drawing.Point(13, 170)
         Me.lblSekundy.Name = "lblSekundy"
         Me.lblSekundy.Size = New System.Drawing.Size(272, 63)
         Me.lblSekundy.TabIndex = 2
         Me.lblSekundy.Text = "S: 000000"
         '
+        'lblPotegi2
+        '
+        Me.lblPotegi2.AutoSize = True
+        Me.lblPotegi2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblPotegi2.Location = New System.Drawing.Point(82, 10)
+        Me.lblPotegi2.Name = "lblPotegi2"
+        Me.lblPotegi2.Size = New System.Drawing.Size(186, 26)
+        Me.lblPotegi2.TabIndex = 3
+        Me.lblPotegi2.Text = "32  16  8   4   2   1"
+        '
         'FormularzGlowny
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(323, 227)
+        Me.ClientSize = New System.Drawing.Size(287, 237)
+        Me.Controls.Add(Me.lblPotegi2)
         Me.Controls.Add(Me.lblSekundy)
         Me.Controls.Add(Me.lblMinuty)
         Me.Controls.Add(Me.lblGodziny)
@@ -75,4 +87,5 @@ Partial Class FormularzGlowny
     Friend WithEvents lblGodziny As Label
     Friend WithEvents lblMinuty As Label
     Friend WithEvents lblSekundy As Label
+    Friend WithEvents lblPotegi2 As Label
 End Class
