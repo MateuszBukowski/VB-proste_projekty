119dc87bd0c403979f7fee227a40bc78f537f671
Mateusz Bukowski <mateuszbukowski13@gmail.com>
2019-10-07 14:43:57 +0200

    [Stoper HEX] - utworzyłem GUI, dodałęm funkcje, jestem w trakcie ich modyfikacji.

---
+++ Stoper HEX/Stoper HEX/FormularzGlowny.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormularzGlowny
+    Inherits System.Windows.Forms.Form
+
+    'Formularz przesłania metodę dispose, aby wyczyścić listę składników.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wymagane przez Projektanta formularzy systemu Windows
+    Private components As System.ComponentModel.IContainer
+
+    'UWAGA: następująca procedura jest wymagana przez Projektanta formularzy systemu Windows
+    'Możesz to modyfikować, używając Projektanta formularzy systemu Windows. 
+    'Nie należy modyfikować za pomocą edytora kodu.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.lblGodziny = New System.Windows.Forms.Label()
+        Me.lblMinuty = New System.Windows.Forms.Label()
+        Me.lblSekundy = New System.Windows.Forms.Label()
+        Me.lblMiliSekundy = New System.Windows.Forms.Label()
+        Me.lblPotega16 = New System.Windows.Forms.Label()
+        Me.TmrCzas = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(244, 85)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(244, 149)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(75, 23)
+        Me.btnStop.TabIndex = 1
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(244, 217)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 2
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'lblGodziny
+        '
+        Me.lblGodziny.AutoSize = True
+        Me.lblGodziny.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblGodziny.Location = New System.Drawing.Point(54, 74)
+        Me.lblGodziny.Name = "lblGodziny"
+        Me.lblGodziny.Size = New System.Drawing.Size(155, 63)
+        Me.lblGodziny.TabIndex = 9
+        Me.lblGodziny.Text = "H: 00"
+        '
+        'lblMinuty
+        '
+        Me.lblMinuty.AutoSize = True
+        Me.lblMinuty.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblMinuty.Location = New System.Drawing.Point(48, 137)
+        Me.lblMinuty.Name = "lblMinuty"
+        Me.lblMinuty.Size = New System.Drawing.Size(161, 63)
+        Me.lblMinuty.TabIndex = 10
+        Me.lblMinuty.Text = "M: 00"
+        '
+        'lblSekundy
+        '
+        Me.lblSekundy.AutoSize = True
+        Me.lblSekundy.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblSekundy.Location = New System.Drawing.Point(54, 200)
+        Me.lblSekundy.Name = "lblSekundy"
+        Me.lblSekundy.Size = New System.Drawing.Size(152, 63)
+        Me.lblSekundy.TabIndex = 11
+        Me.lblSekundy.Text = "S: 00"
+        '
+        'lblMiliSekundy
+        '
+        Me.lblMiliSekundy.AutoSize = True
+        Me.lblMiliSekundy.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblMiliSekundy.Location = New System.Drawing.Point(12, 263)
+        Me.lblMiliSekundy.Name = "lblMiliSekundy"
+        Me.lblMiliSekundy.Size = New System.Drawing.Size(197, 63)
+        Me.lblMiliSekundy.TabIndex = 12
+        Me.lblMiliSekundy.Text = "mS: 00"
+        '
+        'lblPotega16
+        '
+        Me.lblPotega16.AutoSize = True
+        Me.lblPotega16.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblPotega16.Location = New System.Drawing.Point(106, 19)
+        Me.lblPotega16.Name = "lblPotega16"
+        Me.lblPotega16.Size = New System.Drawing.Size(96, 31)
+        Me.lblPotega16.TabIndex = 13
+        Me.lblPotega16.Text = "256 16"
+        '
+        'TmrCzas
+        '
+        Me.TmrCzas.Interval = 1000
+        '
+        'FormularzGlowny
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(380, 352)
+        Me.Controls.Add(Me.lblPotega16)
+        Me.Controls.Add(Me.lblMiliSekundy)
+        Me.Controls.Add(Me.lblSekundy)
+        Me.Controls.Add(Me.lblMinuty)
+        Me.Controls.Add(Me.lblGodziny)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Name = "FormularzGlowny"
+        Me.Text = "Stoper HEX"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lblGodziny As Label
+    Friend WithEvents lblMinuty As Label
+    Friend WithEvents lblSekundy As Label
+    Friend WithEvents lblMiliSekundy As Label
+    Friend WithEvents lblPotega16 As Label
+    Friend WithEvents TmrCzas As Timer
+End Class
---
+++ Stoper HEX/Stoper HEX/FormularzGlowny.vb
@@ -0,0 +1,150 @@
+﻿Public Class FormularzGlowny
+    Private intMiliSekundy As Integer = 0
+    Private intSekundy As Integer = 0
+    Private intMinuty As Integer = 0
+    Private intGodziny As Integer = 0
+    Private dblWynik As Double
+    Private strWynikDecHex As String
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        lblGodziny.Text = "H: 00"
+        lblMinuty.Text = "M: 00"
+        lblSekundy.Text = "S: 00"
+        lblMiliSekundy.Text = "mS: 000"
+    End Sub
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        TmrCzas.Enabled = True
+    End Sub
+    Private Sub BtnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        TmrCzas.Enabled = False
+    End Sub
+    Private Sub TmrCzas_Tick(sender As Object, e As EventArgs) Handles TmrCzas.Tick
+        ZwiekszMiliSekundy()
+    End Sub
+    Private Sub ZwiekszMiliSekundy()
+        Dim strMiliSekundyHEX As String
+        If intMiliSekundy <= 100 Then
+            intMiliSekundy = intMiliSekundy + 1
+            strMiliSekundyHEX = KonwertujDecNaHex(CStr(intMiliSekundy))
+            'strMiliSekundyHEX = SprawdzZera(strMiliSekundyHEX)
+            lblMiliSekundy.Text = "mS: " & strMiliSekundyHEX
+        Else
+            intMiliSekundy = 0
+            ZwiekszSekundy()
+        End If
+    End Sub
+    Private Sub ZwiekszSekundy()
+        Dim strSekundyHEX As String
+        If intSekundy <= 60 Then
+            intSekundy = intSekundy + 1
+            strSekundyHEX = KonwertujDecNaHex(CStr(intSekundy))
+            'strSekundyHEX = SprawdzZera(strSekundyHEX)
+            lblSekundy.Text = "S: " & strSekundyHEX
+        Else
+            intSekundy = 0
+            ZwiekszMinuty()
+        End If
+    End Sub
+    Private Sub ZwiekszMinuty()
+        Dim strMinutyHEX As String
+        If intMinuty <= 60 Then
+            intMinuty = intMinuty + 1
+            strMinutyHEX = KonwertujDecNaHex(CStr(intMinuty))
+            'strMinutyHEX = SprawdzZera(strMinutyHEX)
+            lblMinuty.Text = "M: " & strMinutyHEX
+        Else
+            intMinuty = 0
+            ZwiekszGodziny()
+        End If
+    End Sub
+    Private Sub ZwiekszGodziny()
+        Dim strGodzinyHEX As String
+        If intGodziny <= 60 Then
+            intGodziny = intGodziny + 1
+            strGodzinyHEX = KonwertujDecNaHex(CStr(intGodziny))
+            'strGodzinyHEX = SprawdzZera(strGodzinyHEX)
+            lblGodziny.Text = "H: " & strGodzinyHEX
+        Else
+            TmrCzas.Enabled = False
+        End If
+    End Sub
+    Private Function ZnajdzNajwiekszaPotege16(ByRef dblLiczba As Double) As Double
+        Dim dblPotega16 As Double
+        Dim dblWynikPotegi As Double
+        Dim dblMaxPotega As Double
+        dblPotega16 = 0
+        dblWynikPotegi = 16 ^ dblPotega16
+        dblMaxPotega = 0
+        Do While dblWynikPotegi <= dblLiczba
+            dblMaxPotega = dblPotega16
+            Debug.Write("dblMaxPotega: ")
+            Debug.WriteLine(dblMaxPotega)
+            dblPotega16 = dblPotega16 + 1
+            dblWynikPotegi = 16 ^ dblPotega16
+        Loop
+        Return dblMaxPotega
+    End Function
+    Private Function KonwertujDecNaHex(ByRef strLiczbaDec As String) As String
+        Dim dblOblicz As Double
+        Dim strGotowyWynik As String
+        Dim dblMaxPotega As Double
+        Debug.WriteLine("**************************************")
+        strGotowyWynik = ""
+        dblOblicz = CDbl(strLiczbaDec)
+        Do While dblOblicz >= 16
+            dblMaxPotega = ZnajdzNajwiekszaPotege16(dblOblicz)
+            dblWynik = dblOblicz / (16 ^ dblMaxPotega)
+            Debug.Write("dblWynik: ")
+            Debug.WriteLine(dblWynik)
+            dblWynik = Math.Truncate(dblWynik)
+            Debug.Write("dblWynik: ")
+            Debug.WriteLine(dblWynik)
+            strGotowyWynik = strGotowyWynik & KonwertujWynikDecNaHex(dblWynik)
+            dblOblicz = dblOblicz - (dblWynik * (16 ^ dblMaxPotega))
+        Loop
+        'Wstaw resztę
+        If dblOblicz <= 16 Then
+            dblWynik = CDbl(strLiczbaDec)
+            strGotowyWynik = KonwertujWynikDecNaHex(dblOblicz)
+        Else
+            strGotowyWynik = strGotowyWynik & KonwertujWynikDecNaHex(dblOblicz)
+        End If
+        Return strGotowyWynik
+    End Function
+    Private Function KonwertujWynikDecNaHex(ByRef dblDec As Double) As String
+        Select Case dblDec
+            Case 1, 2, 3, 4, 5, 6, 7, 8, 9
+                strWynikDecHex = CStr(dblWynik)
+            Case 10
+                strWynikDecHex = "A"
+            Case 11
+                strWynikDecHex = "B"
+            Case 12
+                strWynikDecHex = "C"
+            Case 13
+                strWynikDecHex = "D"
+            Case 14
+                strWynikDecHex = "E"
+            Case 15
+                strWynikDecHex = "F"
+            Case 16
+                strWynikDecHex = "G"
+        End Select
+        Return strWynikDecHex
+    End Function
+    Private Function SprawdzZera(ByVal strCzas As String) As String
+        Dim intIloscZnakow As Integer = 0
+        Dim intIleZerDodac As Integer = 0
+        Dim strCzasUstawioneZera As String = ""
+        intIloscZnakow = CInt(Len(strCzas))
+        If intIloscZnakow < 2 Then
+            intIleZerDodac = 2 - intIloscZnakow
+            strCzasUstawioneZera = strCzas
+            For index As Integer = intIleZerDodac To 1 Step -1
+                strCzasUstawioneZera = "0" & strCzasUstawioneZera
+            Next
+        Else
+            strCzasUstawioneZera = strCzas
+        End If
+        Return strCzasUstawioneZera
+    End Function
+End Class
